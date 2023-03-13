d6d3ac300dbdf43405eac1d472120e3df9d15128
Miroslav Kozma <xkozma@stuba.sk>
2023-03-13 20:58:56 +0100

    Camera movement added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+     
+    public float lookSpeedH = 2f;
+    public float lookSpeedV = 2f;
+    public float zoomSpeed = 2f;
+    public float dragSpeed = 6f;
+     
+    private float yaw = 0f;
+    private float pitch = 0f;
+         
+    void Update ()
+    {
+        //Look around with Right Mouse
+        if (Input.GetMouseButton(1))
+        {
+            yaw += lookSpeedH * Input.GetAxis("Mouse X");
+            pitch -= lookSpeedV * Input.GetAxis("Mouse Y");
+     
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        }
+     
+        //drag camera around with Middle Mouse
+        if (Input.GetMouseButton(2))
+        {
+            transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed,   -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
+        }
+     
+        //Zoom in and out with Mouse Wheel
+        transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, zoomSpeed*Time.deltaTime, Space.Self);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -zoomSpeed*Time.deltaTime, Space.Self);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-zoomSpeed*Time.deltaTime, 0, 0, Space.Self);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(zoomSpeed*Time.deltaTime, 0, 0, Space.Self);
+        }
+    }
+}
